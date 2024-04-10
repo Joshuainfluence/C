5281eb9bc8c970886cd4571fdb0109313157c430
unknown <joshuajulius2030@gmail.com>
2024-04-10 10:26:34 +0100

    successful

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char name[] = "influence";
+    int age = 18;
+    char job[] = "Software Developer and a Hacker";
+    char institution[] = "Delta State University Abraka";
+    char grade = 'A';
+    double cgpa = 4.50;
+    printf("There is a man named %s\n", name);
+    printf("He is %d years of age\n", age);
+    printf("He is a %s\n", job);
+    printf("He is currently studying at %s\n", institution);
+    printf("His current grade is %c because he has a CGPA of %f\n", grade, cgpa);
+
+
+    return 0;
+}
